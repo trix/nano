1df4fd19021210135f03ed74ed7ac02701d446c3
Tomasz Mazur <defkode@gmail.com>
2013-05-20 15:34:58 +0200

    Update nano.js

--- nano.js
+++ nano.js
@@ -3,7 +3,7 @@
 function nano(template, data) {
   return template.replace(/\{([\w\.]*)\}/g, function(str, key) {
     var keys = key.split("."), v = data[keys.shift()];
-    for (i = 0, l = keys.length; i < l; _i++) v = v[this];
+    for (i = 0, l = keys.length; i < l; i++) v = v[this];
     return (typeof v !== "undefined" && v !== null) ? v : "";
   });
 };
