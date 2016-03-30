167cb0fa05d784dd75a41d68b442ead9f8558dde
azettl <info@azettl.net>
2016-03-30 19:46:59 +0530

    documentation and clean up

--- nano.js
+++ nano.js
@@ -1,13 +1,21 @@
 /*
   Nano Templates - https://github.com/trix/nano
                  - https://github.com/azettl/nano
-*/
 
-function nano(template, data, showUndefined) {
+  Used to replace text like {user.lastname} with data from an json
+  object like {user: {lastname: 'demo'}}.
+
+  @param string  t   the template / string in which the replacement should happen.
+  @param object  d   the json object.
+  @param boolean u   whether strings which can't be replaced should be shown.
+
+  @return string     the replaced template / string.
+*/
+function nano(t, d, u) {
   "use strict";
-  return template.replace(/\{([\w\.]*)\}/g, function (str, key) {
-    var keys = key.split("."), v = data[keys.shift()];
-    keys.forEach(function (e) { v = v[e]; });
-    return (typeof v !== "undefined" && v !== null) ? v : (showUndefined ? str : "");
+  return t.replace(/\{([\w\.]*)\}/g, function (s, k) {
+    var p = k.split("."), v = d[p.shift()];
+    p.forEach(function (e) { v = v[e]; });
+    return (typeof v !== "undefined" && v !== null) ? v : (u ? s : "");
   });
 }
--- readme.md
+++ readme.md
@@ -1,7 +1,29 @@
 NANO - Template Engine
 =============================
 
-***Basic Usage***
+***nano()***
+
+The *nano()* method replaces text like {user.lastname} with data from an JSON object like {user: {lastname: 'demo'}}.
+
+**Syntax**
+
+<pre>nano(t, d[, u])</pre>
+
+
+**Parameters**
+
+  *t* (string)   
+  The template / string in which the replacement should happen.
+
+  *d* (object)   
+  The JSON object.
+
+  *u* (boolean)  
+  Optional. Whether strings which can't be replaced should be shown.
+
+
+
+***Examples***
 
 Assuming you have following JSON response:
 
@@ -42,6 +64,7 @@ and you get the following string:
 <code>
   &lt;p&gt;Hello Thomas Mazur! Your account is &lt;strong&gt;active&lt;/strong&gt; {user.nonexistingnode}&lt;/p&gt;
 </code>
+
 
 
 The test page can be found here: <a href="testPage.html">testPage.html</a>
