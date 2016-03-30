62fd402d2f88f36255b83a9574a08389e138d0f0
azettl <info@azettl.net>
2016-03-30 18:43:17 +0530

    missed line in commit

--- nano.js
+++ nano.js
@@ -3,6 +3,7 @@
 function nano(template, data, showUndefined) {
   return template.replace(/\{([\w\.]*)\}/g, function(str, key) {
     var keys = key.split("."), v = data[keys.shift()];
+    for (var i = 0, l = keys.length; i < l; i++){ v = v[keys[i]]; k = str; }
     return (typeof v !== "undefined" && v !== null) ? v : (showUndefined ? k : "");
   });
 }
